4215f245ed85e717d1a5672ab4a43eac6aab4b27
BuildTools <unconfigured@null.spigotmc.org>
2019-09-13 09:37:34 -0500

    began inplementation

--- Shapes/Program.cs
+++ Shapes/Program.cs
@@ -12,4 +12,56 @@ namespace Shapes
         {
         }
     }
+    
+
+
+    class ShapeFactory : GeometricShape
+    {
+        public GeometricShape getShape(ShapeType type)
+        {
+
+        }
+
+        void GeometricShape.Draw()
+        {
+
+        }
+    }
+
+    enum ShapeType
+    {
+        LINE,
+        CIRCLE,
+        RECTANGLE,
+        TRIANGLE
+    }
+
+    interface GeometricShape
+    {
+        void Draw();
+    }
+
+    class Line : GeometricShape
+    {
+        void GeometricShape.Draw()
+        {
+            Console.WriteLine("Line is drawn.");
+        }
+    }
+
+    class Circle : GeometricShape
+    {
+        void GeometricShape.Draw()
+        {
+            Console.WriteLine("Circle is drawn.");
+        }
+    }
+
+    class Rectangle : GeometricShape
+    {
+        void GeometricShape.Draw()
+        {
+            Console.WriteLine("Rectangle is drawn.");
+        }
+    }
 }
